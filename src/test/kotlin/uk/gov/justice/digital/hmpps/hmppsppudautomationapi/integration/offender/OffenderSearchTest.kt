d6148ed85bd431bc0b94cf715c11fc1390a18c0a
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-11-09 14:54:27 +0000

    MRD-2004 Bit more testing

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/integration/offender/OffenderSearchTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/integration/offender/OffenderSearchTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.BodyInserters
 import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.integration.IntegrationTestBase
+import java.time.LocalDate
 import java.util.function.Consumer
 
 class OffenderSearchTest : IntegrationTestBase() {
@@ -219,10 +220,11 @@ class OffenderSearchTest : IntegrationTestBase() {
   }
 
   @Test
-  fun `given croNumber of existing offender when search called then offender details are returned`() {
+  fun `given CRO number of existing offender when search called then offender details are returned`() {
     val croNumber = "5159/08A"
     val firstNames = "John"
     val familyName = "Teal"
+    val id = "4F6666656E64657269643D313731383134G721H664"
     val requestBody = "{ " +
       "\"croNumber\": \"$croNumber\"" +
       "}"
@@ -234,8 +236,82 @@ class OffenderSearchTest : IntegrationTestBase() {
       .expectStatus()
       .isOk
       .expectBody()
+      .jsonPath("results[0].id").isEqualTo(id)
       .jsonPath("results[0].croNumber").isEqualTo(croNumber)
       .jsonPath("results[0].firstNames").isEqualTo(firstNames)
       .jsonPath("results[0].familyName").isEqualTo(familyName)
+  }
+
+  @Test
+  fun `given NOMS ID of existing offender when search called then offender details are returned`() {
+    val nomsId = "A1122BC"
+    val firstNames = "Gary"
+    val familyName = "Teal"
+    val id = "4F6666656E64657269643D313731383135G722H664"
+    val requestBody = "{ " +
+      "\"nomsId\": \"$nomsId\"" +
+      "}"
+    webTestClient.post()
+      .uri("/offender/search")
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(BodyInserters.fromValue(requestBody))
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("results[0].id").isEqualTo(id)
+      .jsonPath("results[0].nomsId").isEqualTo(nomsId)
+      .jsonPath("results[0].firstNames").isEqualTo(firstNames)
+      .jsonPath("results[0].familyName").isEqualTo(familyName)
+  }
+
+  @Test
+  fun `given family name and date of birth of existing offender when search called then offender details are returned`() {
+    val dateOfBirth = LocalDate.parse("1902-12-12")
+    val firstNames = "Duncan"
+    val familyName = "Teal"
+    val id = "4F6666656E64657269643D313731383136G723H664"
+    val requestBody = "{ " +
+      "\"familyName\": \"$familyName\"," +
+      "\"dateOfBirth\": \"$dateOfBirth\"" +
+      "}"
+    webTestClient.post()
+      .uri("/offender/search")
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(BodyInserters.fromValue(requestBody))
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("results[0].id").isEqualTo(id)
+      .jsonPath("results[0].firstNames").isEqualTo(firstNames)
+      .jsonPath("results[0].familyName").isEqualTo(familyName)
+      .jsonPath("results[0].dateOfBirth").isEqualTo(dateOfBirth.toString())
+  }
+
+  @Test
+  fun `given family name and date of birth of multiple existing offenders when search called then offender details are returned`() {
+    val dateOfBirth = LocalDate.parse("1980-12-31")
+    val familyName = "Teal"
+    val requestBody = "{ " +
+      "\"familyName\": \"$familyName\"," +
+      "\"dateOfBirth\": \"$dateOfBirth\"" +
+      "}"
+    webTestClient.post()
+      .uri("/offender/search")
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(BodyInserters.fromValue(requestBody))
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("results[0].id").isEqualTo("4F6666656E64657269643D313731383138G725H664")
+      .jsonPath("results[0].firstNames").isEqualTo("Jane")
+      .jsonPath("results[0].familyName").isEqualTo(familyName)
+      .jsonPath("results[0].dateOfBirth").isEqualTo(dateOfBirth.toString())
+      .jsonPath("results[1].id").isEqualTo("4F6666656E64657269643D313731383139G726H664")
+      .jsonPath("results[1].firstNames").isEqualTo("Emily")
+      .jsonPath("results[1].familyName").isEqualTo(familyName)
+      .jsonPath("results[1].dateOfBirth").isEqualTo(dateOfBirth.toString())
   }
 }
