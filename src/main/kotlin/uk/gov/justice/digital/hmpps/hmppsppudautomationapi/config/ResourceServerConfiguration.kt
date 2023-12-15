73f152d77838afb7388de1de28f0ab866c24897c
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-12-15 09:08:39 +0000

    Address linting warnings

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/config/ResourceServerConfiguration.kt
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
