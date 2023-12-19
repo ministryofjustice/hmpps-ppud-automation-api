5ef4352bf725875eceff22ae8d27087ad59719f6
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2023-12-19 11:48:20 +0000

    Add docker compose to build scripts

--- build.sh
+++ build.sh
@@ -2,5 +2,12 @@
 
 set -euo pipefail
 
+if ! docker info >/dev/null 2>&1; then
+  echo "Docker isn't running.  Please start docker and try again"
+  exit 1
+fi
+
+docker compose up -d redis
+
 ./gradlew ktlintFormat
 ./gradlew clean check
