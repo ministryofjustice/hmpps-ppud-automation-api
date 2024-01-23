2022f524a5c8b2c5e7af4aa4b3c0e6822ca60863
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2024-01-23 10:54:50 +0000

    MRD-2209 Bump Selenium version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("org.seleniumhq.selenium:selenium-java:4.16.1")
+  implementation("org.seleniumhq.selenium:selenium-java:4.17.0")
   implementation("io.github.bonigarcia:webdrivermanager:5.6.3")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
