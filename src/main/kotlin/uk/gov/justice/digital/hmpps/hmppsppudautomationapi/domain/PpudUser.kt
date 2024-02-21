e6f939c6bfe57ee9c9c4c8faea552ac082979255
Darren Walker <Darren.Walker@digital.justice.gov.uk>
2024-02-21 11:40:20 +0000

    Hide formattedFullNameAndTeam from Swagger

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/domain/PpudUser.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/domain/PpudUser.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain
 
+import io.swagger.v3.oas.annotations.Hidden
 import jakarta.validation.constraints.NotBlank
 
 data class PpudUser(
@@ -8,6 +9,7 @@ data class PpudUser(
   @field:NotBlank
   val teamName: String,
 ) {
+  @get:Hidden
   internal val formattedFullNameAndTeam: String
     get() = "${this.fullName}(${this.teamName})"
 }
