320f54dc81a72cd4212a35c6287956aeee6c6b03
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-05-16 12:27:13 +0100

    MRD-2684 Introduce post-release client

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/ppud/client/postrelease/PostReleaseClientTest.kt
@@ -0,0 +1,37 @@
+package uk.gov.justice.digital.hmpps.hmppsppudautomationapi.ppud.client.postrelease
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.then
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.request.updatePostReleaseRequest
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.ppud.pages.PostReleasePage
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.testdata.randomString
+
+@ExtendWith(MockitoExtension::class)
+internal class PostReleaseClientTest {
+
+  @InjectMocks
+  private lateinit var client: PostReleaseClient
+
+  @Mock
+  private lateinit var postReleasePage: PostReleasePage
+
+  @Test
+  fun `update post release`() {
+    // given
+    val releaseId = randomString()
+    val updatePostReleaseRequest = updatePostReleaseRequest()
+
+    // when
+    client.updatePostRelease(releaseId, updatePostReleaseRequest)
+
+    // then
+    then(postReleasePage).should().navigateToPostReleaseFor(releaseId)
+    then(postReleasePage).should().updatePostRelease(updatePostReleaseRequest)
+    then(postReleasePage).should().throwIfInvalid()
+  }
+
+}
