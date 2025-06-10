7d59eb5f3e3b30de1dd0bff1ef84768c6eb37b94
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-06-10 14:53:39 +0100

    MRD-2684 Set recall type based on config
    The Recall Type on a Recall created through the service should be set to one of
    two values based on the relevant sentence's custody type. The existing code is
    here extended to support the indeterminate case.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/domain/offender/Sentence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/domain/offender/Sentence.kt
@@ -1,7 +1,10 @@
 package uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.offender
 
-import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.offender.ReleasedUnder.IPP_LICENCE
-import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.offender.ReleasedUnder.LIFE_LICENCE
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.offender.SupportedReleasedUnder.IPP_LICENCE
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.offender.SupportedReleasedUnder.LIFE_LICENCE
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.recall.SupportedRecallType
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.recall.SupportedRecallType.DETERMINATE_RECALL
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.recall.SupportedRecallType.INDETERMINATE_RECALL
 import java.time.LocalDate
 
 data class Sentence(
@@ -26,16 +29,20 @@ data class Sentence(
  * updating sentences. There are more types in PPUD which are ignored in one way
  * or another (if not directly in this service, in the API or UI services).
  */
-enum class SupportedCustodyType(val fullName: String, val releasedUnder: ReleasedUnder?) {
-  DETERMINATE("Determinate", null),
-  EDS("EDS", null),
-  EDS_NON_PAROLE("EDS (non parole)", null),
-  IPP("IPP", IPP_LICENCE),
-  DPP("DPP", IPP_LICENCE),
-  MANDATORY_MLP("Mandatory (MLP)", LIFE_LICENCE),
-  DISCRETIONARY("Discretionary", LIFE_LICENCE),
-  DISCRETIONARY_TARIFF_EXPIRED("Discretionary (Tariff Expired)", LIFE_LICENCE),
-  AUTOMATIC("Automatic", LIFE_LICENCE),
+enum class SupportedCustodyType(
+  val fullName: String,
+  val releasedUnder: SupportedReleasedUnder?,
+  val recallType: SupportedRecallType,
+) {
+  DETERMINATE("Determinate", null, DETERMINATE_RECALL),
+  EDS("EDS", null, DETERMINATE_RECALL),
+  EDS_NON_PAROLE("EDS (non parole)", null, DETERMINATE_RECALL),
+  IPP("IPP", IPP_LICENCE, INDETERMINATE_RECALL),
+  DPP("DPP", IPP_LICENCE, INDETERMINATE_RECALL),
+  MANDATORY_MLP("Mandatory (MLP)", LIFE_LICENCE, INDETERMINATE_RECALL),
+  DISCRETIONARY("Discretionary", LIFE_LICENCE, INDETERMINATE_RECALL),
+  DISCRETIONARY_TARIFF_EXPIRED("Discretionary (Tariff Expired)", LIFE_LICENCE, INDETERMINATE_RECALL),
+  AUTOMATIC("Automatic", LIFE_LICENCE, INDETERMINATE_RECALL),
   ;
 
   override fun toString(): String = fullName
