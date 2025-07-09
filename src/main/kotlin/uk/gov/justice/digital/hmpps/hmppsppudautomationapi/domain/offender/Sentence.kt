b6a84a73154f6b07ecb311528cdc4a9a8d32b883
Pablo Mascaraque Glenn <pablo.glenn@digital.justice.gov.uk>
2025-07-09 17:06:48 +0100

    MRD-2687 Add new custody type reference endpoints (#290)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/domain/offender/Sentence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/domain/offender/Sentence.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.offender
 
+import uk.gov.justice.digital.hmpps.hmppsppudautomationapi.domain.offender.CustodyGroup.INDETERMINATE
 import java.time.LocalDate
 
 data class Sentence(
@@ -17,3 +18,35 @@ data class Sentence(
   val sentenceLength: SentenceLength? = null,
   val sentencingCourt: String,
 )
+
+/**
+ * This enum refers to the custody types supported by CaR in terms of creating and
+ * updating sentences. There are more types in PPUD which are ignored in one way
+ * or another (if not directly in this service, in the API or UI services).
+ */
+enum class SupportedCustodyType(
+  val fullName: String,
+  val custodyGroup: CustodyGroup,
+) {
+  DETERMINATE("Determinate", CustodyGroup.DETERMINATE),
+  EDS("EDS", CustodyGroup.DETERMINATE),
+  EDS_NON_PAROLE("EDS (non parole)", CustodyGroup.DETERMINATE),
+  DPP("DPP", INDETERMINATE),
+  DISCRETIONARY("Discretionary", INDETERMINATE),
+  DISCRETIONARY_TARIFF_EXPIRED("Discretionary (Tariff Expired)", INDETERMINATE),
+  IPP("IPP", INDETERMINATE),
+  MANDATORY_MLP("Mandatory (MLP)", INDETERMINATE),
+  AUTOMATIC("Automatic", INDETERMINATE),
+  ;
+
+  override fun toString(): String = fullName
+
+  companion object {
+    fun forFullName(fullName: String): SupportedCustodyType = entries.first { it.fullName == fullName }
+  }
+}
+
+enum class CustodyGroup(val fullName: String) {
+  DETERMINATE("Determinate"),
+  INDETERMINATE("Indeterminate"),
+}
