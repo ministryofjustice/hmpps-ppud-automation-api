a15ffd0ba28e079c98b050c329a58b18935744f4
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-07-17 10:28:32 +0100

    Upgrade HMPPS gradle plug-in

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.2"
   kotlin("plugin.spring") version "2.1.21"
   id("org.sonarqube") version "6.2.0.5505"
   id("jacoco")
