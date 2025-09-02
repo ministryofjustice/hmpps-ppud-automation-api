99948bc8a6c1ecfefba17ab59ac1ad582407db43
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-09-02 15:26:43 +0100

    MRD-2712 Enable bearer auth in Swagger UI
    This will allow us to call the endpoints from Swagger UI (assuming we do so
    with the auth token).

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/config/OpenApiConfiguration.kt
@@ -1,8 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsppudautomationapi.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
@@ -26,4 +29,16 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
         .description("API to control the automation of PPUD frontend via Selenium")
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
     )
+    .components(
+      Components().addSecuritySchemes(
+        "bearer-jwt",
+        SecurityScheme()
+          .type(SecurityScheme.Type.HTTP)
+          .scheme("bearer")
+          .bearerFormat("JWT")
+          .`in`(SecurityScheme.In.HEADER)
+          .name("Authorization"),
+      ),
+    )
+    .addSecurityItem(SecurityRequirement().addList("bearer-jwt"))
 }
