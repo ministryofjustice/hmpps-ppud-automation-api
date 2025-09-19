8da1d35c4775fb0e39b930b3de79616619159f0b
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-09-19 18:12:47 +0100

    MRD-2780 Ongoing migration to GHA

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/HmppsPpudAutomationApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsppudautomationapi/HmppsPpudAutomationApi.kt
@@ -13,3 +13,12 @@ class HmppsPpudAutomationApi
 fun main(args: Array<String>) {
   runApplication<HmppsPpudAutomationApi>(*args)
 }
+
+These are notes on the migration, to be added to the commit message. Reject if I
+forgot to remove them from here (the application should fail to compile anyway).
+
+SonarQube is already due to lose support at some point, as was announced in Slack
+a while back, so we've decided to use this migration to remove it altogether. With
+this, we also remove jacoco, which was generating the data for SonarQube. We haven't
+been doing separate checks on the code coverage data jacoco produces, so there's no
+point in keeping it. If we do need it in the future we can always re-add it.
