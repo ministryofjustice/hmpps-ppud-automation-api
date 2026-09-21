d19dfdee24701e855bf302cae9b351f939ca4155
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2026-09-21 12:11:50 +0100

    MRD-3389 Address CVEs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.9"
   kotlin("plugin.spring") version "2.4.10"
 }
 
@@ -26,12 +26,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
   implementation("org.springframework.boot:spring-boot-jackson2")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.21.6") {
-    because("Address CVE-2026-19032 & CVE-2026-68497")
-  }
-  implementation("tools.jackson.core:jackson-databind:3.1.6") {
-    because("Address CVE-2026-19032 & CVE-2026-68497")
-  }
   implementation("ch.qos.logback:logback-classic:1.6.3") {
     because("Address CVE-2026-19880")
   }
